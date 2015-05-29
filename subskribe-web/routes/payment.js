1a15b6335acbcb370978adc3e51406ffc1d399d0
BipinGhadi <bipin@appliedcloudcomputing.com>
2015-05-29 18:13:15 +0530

    payment

--- subskribe-web/routes/payment.js
+++ subskribe-web/routes/payment.js
@@ -15,7 +15,28 @@ router.get('/', function(req, res, next) {
        address : currentUser.get("address"),
 
     }
-      res.render('payment', {user : _user});
+    var currpkg={};
+    var SavePackage = Parse.Object.extend("SavePackage");
+    var query = new Parse.Query(SavePackage);
+    query.equalTo("email", _user.email);
+    query.first({
+      success: function(results) {
+      console.log("In Success");
+          currpkg={
+          currpkgname : results.get('packname'),
+          currpkgvalidity : results.get('packvalidity'),
+          currpkgprice : results.get('packprice')
+                }
+          console.log(JSON.stringify(currpkg));
+          res.render('payment', {user : _user, currpkg : currpkg});
+                },
+
+          error: function(error) {
+          console.log("In Error");
+  }
+});
+
+      
 
   } else {
       // show the signup or login page
